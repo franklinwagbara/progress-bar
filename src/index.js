b1e48fba3ef401544459820b229fb588dc078abe
Franklin Wagbara <wagbarafranklin@yahoo.com>
2022-02-10 20:56:28 +0100

    Removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -19,41 +19,6 @@ ReactDOM.render(
 
 // // setInterval(tick, 1000);
 
-// class Clock extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { date: new Date() };
-//   }
-
-//   componentDidMount() {
-//     this.timerId = setInterval(() => this.tick(), 100);
-//   }
-
-//   componentWillUnmount() {
-//     clearInterval(this.timerId);
-//   }
-
-//   tick() {
-//     this.setState((this.state = { date: new Date() }));
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>Time: {this.state.date.toLocaleTimeString()}</h1>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Clock;
-
-// reactDom.render(
-//   <React.StrictMode>
-//     <Clock />
-//     <Game />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
